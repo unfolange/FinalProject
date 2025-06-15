5f1cd472ee7d04c2aec612c443cbf425cb1cf65b
Yersonelgris <yersonjsanchez@gmail.com>
2025-06-14 21:17:46 -0500

    UIScenes
    corregidas las 3 escenas de UI

--- Assets/Scripts/UI/Canvas/NoButton.cs
+++ Assets/Scripts/UI/Canvas/NoButton.cs
@@ -4,12 +4,18 @@ public class NoButton : MonoBehaviour
 {
     public GameObject desGame;
     public GameObject mainMenu;
+    public GameObject continuePanel;
 
     public void DesactiveGame()
     {
         // Mostrar el nuevo panel
         desGame.SetActive(false);
 
+    }
+
+    public void DesContinue()
+    {
+        continuePanel.SetActive(false);
     }
 
     public void ActiveMenu()
